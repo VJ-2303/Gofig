9e271d626af5a2a7780bbccc21907880d617b62a
VJ-2303 <vanaraj1018@gmail.com>
2025-10-24 18:30:16 +0530

    feat: Implemented JSOn file loading and parsing

--- config.go
+++ config.go
@@ -1,7 +1,10 @@
 // Package Gofig provides utilities for loading configuration files.
 package gofig
 
-import "fmt"
+import (
+	"encoding/json"
+	"os"
+)
 
 // Load reads a configuration file from the given path and populates
 // the fields of the provided 'cfg' struct.
@@ -9,6 +12,21 @@ import "fmt"
 // 'filepath' is the path to the configuration file
 // 'cfg' must be a pointer to a struct
 func Load(filepath string, cfg interface{}) error {
-	fmt.Println("Loading config from:", filepath)
+	// Open the configuration file
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	// Ensure the file is closed when the function exists
+	defer file.Close()
+
+	// Create a new JSON decodeer that reads from our file
+	decoder := json.NewDecoder(file)
+
+	// Decode the JSON into the provided 'cfg' struct
+	// 'cfg' must be an Pointer !
+	if err := decoder.Decode(cfg); err != nil {
+		return err
+	}
 	return nil
 }
--- config_test.go
+++ config_test.go
@@ -7,15 +7,23 @@ import (
 )
 
 // TestLoad is the test function for our Load function.
-func TestLoad(t *testing.T) {
+func TestLoad_JSON(t *testing.T) {
 	type AppConfig struct {
-		Port int
-		Host string
+		Port int    `json:"port"`
+		Host string `json:"host"`
 	}
 	var cfg AppConfig
 
-	err := gofig.Load("dummy/path.json", &cfg)
+	filepath := "testdata/config.json"
+	err := gofig.Load(filepath, &cfg)
 	if err != nil {
-		t.Errorf("Load() returned an unexpected error: %v", err)
+		t.Fatalf("Load() returned an unexpected error: %v", err)
+	}
+
+	if cfg.Port != 8080 {
+		t.Errorf("expected Port to be 8080, but go %d", cfg.Port)
+	}
+	if cfg.Host != "localhost" {
+		t.Errorf("expected Host to be 'localhost', but go %s", cfg.Host)
 	}
 }
